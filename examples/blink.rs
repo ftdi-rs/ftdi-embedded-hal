bb082df5c346754a3de42225b07293333dcfb8b7
Sergey Matyukevich <geomatsi@gmail.com>
2021-10-10 23:39:57 +0300

    update examples
    Update examples according to HAL changes. Add support for two drivers
    including libftd2xx and libftdi. Use features to switch between drivers.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- examples/blink.rs
+++ examples/blink.rs
@@ -2,15 +2,30 @@ use embedded_hal::digital::v2::OutputPin;
 use ftdi_embedded_hal as hal;
 use std::{thread::sleep, time::Duration};
 
+#[cfg(feature = "libftd2xx")]
+use std::convert::TryInto;
+
 const NUM_BLINK: usize = 10;
 const SLEEP_DURATION: Duration = Duration::from_millis(500);
 
+#[cfg(all(feature = "ftdi", feature = "libftd2xx"))]
+compile_error!("features 'ftdi' and 'libftd2xx' cannot be enabled at the same time");
+
+#[cfg(not(any(feature = "ftdi", feature = "libftd2xx")))]
+compile_error!("one of features 'ftdi' and 'libftd2xx' shall be enabled");
+
 fn main() {
-    let ftdi = hal::Ft232hHal::new()
-        .expect("Failed to open FT232H device")
-        .init_default()
-        .expect("Failed to initialize MPSSE");
-    let mut output_pin = ftdi.ad3();
+    #[cfg(feature = "libftd2xx")]
+    let device: libftd2xx::Ft232h = libftd2xx::Ftdi::new().unwrap().try_into().unwrap();
+
+    #[cfg(feature = "ftdi")]
+    let device = ftdi::find_by_vid_pid(0x0403, 0x6014)
+        .interface(ftdi::Interface::A)
+        .open()
+        .unwrap();
+
+    let hal = hal::FtHal::init_default(device).unwrap();
+    let mut output_pin = hal.ad3();
 
     println!("Starting blinky example");
     for n in 0..NUM_BLINK {
--- examples/bme280.rs
+++ examples/bme280.rs
@@ -10,12 +10,27 @@
 use embedded_hal::prelude::*;
 use ftdi_embedded_hal as hal;
 
+#[cfg(feature = "libftd2xx")]
+use std::convert::TryInto;
+
+#[cfg(all(feature = "ftdi", feature = "libftd2xx"))]
+compile_error!("features 'ftdi' and 'libftd2xx' cannot be enabled at the same time");
+
+#[cfg(not(any(feature = "ftdi", feature = "libftd2xx")))]
+compile_error!("one of features 'ftdi' and 'libftd2xx' shall be enabled");
+
 fn main() {
-    let ftdi = hal::Ft232hHal::new()
-        .expect("Failed to open FT232H device")
-        .init_default()
-        .expect("Failed to initialize MPSSE");
-    let mut i2c = ftdi.i2c().expect("Failed to initialize I2C");
+    #[cfg(feature = "libftd2xx")]
+    let device: libftd2xx::Ft232h = libftd2xx::Ftdi::new().unwrap().try_into().unwrap();
+
+    #[cfg(feature = "ftdi")]
+    let device = ftdi::find_by_vid_pid(0x0403, 0x6014)
+        .interface(ftdi::Interface::A)
+        .open()
+        .unwrap();
+
+    let hal = hal::FtHal::init_default(device).unwrap();
+    let mut i2c = hal.i2c().unwrap();
 
     let mut buf: [u8; 1] = [0];
     const BME280_ADDR: u8 = 0b1110111;
